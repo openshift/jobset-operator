231ceb8c384c5fa2bd3ae0802e9f6d5a62d3fb8e
Arda Güçlü <aguclu@redhat.com>
2025-07-14 10:09:13 +0300

    Run code generators to update clientsets

--- pkg/generated/informers/externalversions/openshiftoperator/v1/jobsetoperator.go
+++ pkg/generated/informers/externalversions/openshiftoperator/v1/jobsetoperator.go
@@ -59,13 +59,25 @@ func NewFilteredJobSetOperatorInformer(client versioned.Interface, resyncPeriod 
 				if tweakListOptions != nil {
 					tweakListOptions(&options)
 				}
-				return client.OpenShiftOperatorV1().JobSetOperators().List(context.TODO(), options)
+				return client.OpenShiftOperatorV1().JobSetOperators().List(context.Background(), options)
 			},
 			WatchFunc: func(options metav1.ListOptions) (watch.Interface, error) {
 				if tweakListOptions != nil {
 					tweakListOptions(&options)
 				}
-				return client.OpenShiftOperatorV1().JobSetOperators().Watch(context.TODO(), options)
+				return client.OpenShiftOperatorV1().JobSetOperators().Watch(context.Background(), options)
+			},
+			ListWithContextFunc: func(ctx context.Context, options metav1.ListOptions) (runtime.Object, error) {
+				if tweakListOptions != nil {
+					tweakListOptions(&options)
+				}
+				return client.OpenShiftOperatorV1().JobSetOperators().List(ctx, options)
+			},
+			WatchFuncWithContext: func(ctx context.Context, options metav1.ListOptions) (watch.Interface, error) {
+				if tweakListOptions != nil {
+					tweakListOptions(&options)
+				}
+				return client.OpenShiftOperatorV1().JobSetOperators().Watch(ctx, options)
 			},
 		},
 		&apisopenshiftoperatorv1.JobSetOperator{},
