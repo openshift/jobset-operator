b3e82b80d4bcd862559cd6cead4442bdc6fdfcce
Arda Güçlü <aguclu@redhat.com>
2026-01-14 09:34:08 +0300

    Prepare for FBC 4.22

--- generate-fbc.sh
+++ generate-fbc.sh
@@ -1,4 +1,4 @@
-for OCP_VERSION in v4.18 v4.19 v4.20 v4.21; do
+for OCP_VERSION in v4.18 v4.19 v4.20 v4.21 v4.22; do
     echo "OCP_VERSION: ${OCP_VERSION}"
     opm alpha render-template semver $OCP_VERSION/catalog-template.yaml --migrate-level=bundle-object-to-csv-metadata > $OCP_VERSION/catalog/jobset-operator/catalog.json;
 done
